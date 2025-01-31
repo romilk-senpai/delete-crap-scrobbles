2fa455c72b2dd2ca5636ce66f8cd43725633024a
romilk-senpai <rpakhomov1702@gmail.com>
2025-02-01 00:08:30 +0200

    yokoso kira kira

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # delete-crap-scrobbles
 Small Python utility to filter and delete scrobbles from Last.fm
+
+Requires premium account to fetch all scrobbles list. If you want it free, you can remake fetching with scrapping.
+
+Put your token and username in the corresponding fields in `delete_crap_scrobbles.py`
+
+Last FM does not provide API endpoint to delete scrobbles, therefore it is done by sending same form as webside sends when you press 'Delete scrobble' button. For this to work you might need to change `CSRFTOKEN` and `SESSIONID` which you can find in `Network` tab in Chrome developer tools. Idk about other browsers.
+
+Also you might need to change `CSRFMIDDLEWARE` which you can find in `Elements` tab in Chrome developer tools if you inspect `Delete scrobble` button and look over the html form it belongs to.
+
+Any Python version will work hopefully because there's no any additional libraries. I personally used 3.10.6
+By default it deletes scrobbles which artist you have listened only once.
+If you want another behaviour edit this function to your needs
+```python
+	def filter_shitty_scrobbles(scrobbles, artist_counts)
+```
+It asks for confirmation before deleting as well, and saves all the information in json format in the script directory such as *how it was before*, *what it is going to delete*, *what it have deleted*, *what it failed to delete*.
---
+++ delete_crap_scrobbles.py
@@ -0,0 +1,157 @@
+import json
+import requests
+import time
+
+API_KEY = 'YOUR_API_KEY'
+USERNAME = 'YOUR_USERNAME'
+
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36'
+CSRFTOKEN = '1UHoPy15DXVMzbh0mAncNM0psDVgQcLf'
+SESSIONID= '.eJyVVtmuq8YS_Zf9HO_DbDhSHrDBNtiAmaFfLKAxg5lsZqL8-228d-6gqyjJgwW4V1Wtqu611L993IK-S299G79uadCmHz8_wmvTno7zsqcpl5a4rWMGqTnp_C7LGSUOXh-_fLRx22Z1dcsgwkMCZ7ckF2xiKg43VBwTm4DdshuOo5kY24YYxrEo5r8KvcN-_vxpm6IhCegFx0iawjEU8L_AMIgecbWii3v5Ocbh5ziOn0HTtJ8r6PN7_dN-tbtvKIpv-leLQhQrmjRLwjRBGdX8QSvJr7-uy-3XcjzLaXiMMi2TTXuRcDWTWqnsGrCXGKmUg4vD4mCWuU8EbCJSWYE1PBljtNTDhVSLiFSbkKB6UBYzcOU0IuweENwAiKm4lOoQmlxzcac0Io0yNPEUekYtZWPme85DyussMPHW91QMlFiH_sOC_UrggMNjeofHogOmxKAeFsR_URceV1As9FDdgvsimSuTavmEaomzKvikYj3QU1xUwUY9J5giPAg110lV4Me1bkQ62VpXWURczX1cQbNR868ZKZaCqZZOaYI-amtOIULxfHbZy43v6ZmWi6SyJIuyRKNqrTwdKtp_87MkSl30Nz_g4mngjuus5uh4eAVHrgtJf0L8Z_0IO98t2q_ZvHO8vz0SFFFZlIG7zqXBosopUN0y8OTCLw_LOod1D8DR6dFcMIRbEG77Rzx6D6VKHn23q0MCH-DKVZB5QLy59D7BdVKBcyjnEriw94ii98jdHJJgzTmvM0Fcl4iEQ1Tqa88YcOk0rPS1DzyqlO_9wceo5B4X1xjgScSlCuc-he65B7xdJCBogRaO93TYsHB6lC8D5qWgMW4f85330sarFFXitXq10_Nq3JUrUSt9fiWTNgKEctFHdQ8lR90s0pUWSi1v9iEsfaew-DN7eh2SpOPctGm2znm-k_Wz7PGgIjq-zfKFtB2KJqkzsOK7VAC-ucV0VbbLhXIwBnvsqcF9PVGmKhxp6Nt9fzZdKT30G5V47VRnYZRjwh5vnSoG0iMbl96f6N1hAIF4AaLGt4bHlodrQI81ZR76w7U-k31HnDRlc_Lvm1GcD41yvtLMQ5t8Nj_4McTFxc3Ke3rg2zoh42d9rO61Hb3YhJtZ7HkbdqY1F-dbBTnSMDc3-NR35_FZKZwya0zOrzLt_oZM82Tyc3tW8kOhEiIOjvYILCcFuU6rhI1U46TqYmSKAB6aIGe-FVHaUXrvcOzxa86zg-vZXf9TkfukMwPPGALC6S8lHEICtsCk85DAhj8T8JeDPLoQHSp4BN3FXUWdrGXfJuCRRhO69v-LqnIW4Mn3wF0PnrQolo0EbE9KrhPqItLoOSlIaKqQUEjUNBIxhoQ3KmvdSsX97J-bQuAeMCQAJFCFUhYFCceeEc8iPvH_5oewxLfovwWHBFPt0oBIh9DlCGQCo_LYVaF7aML9ezZrr8X7-8ARqKcGmWSxzgWeZByYyCj-Y5qZVrWIh1GEldEggRfR_Bb8VzyKcee_MhMZ3lHOiCiqEJkj-q1CJdac0ToTxBWZNRmS8mPtGSIj8V167RsZqrx8708XneQB7PFHSBqTasocOhaXgu4tpSlOh-k561oDzZio-i1_tZ8erF7yE-wuhhNGW2dfPxvMZKfBf7Cvy6uUi9o7mnIQR1n56j3oP_M9xcm-a5KFRsOt04GyYSPZIjwy0G2afu6mp4ZNKQYL3IiVnWTl0eliG5ZA-QnkIK1blDhWe6t3hJOxE8ReE0MLDFcum8jz1ZcU3BWuWO-N5Ya_aMJe5EB3buiNLMXDjZwsh042-0XONu49YBnOZkFeG6A2W-qqs_Gw2TG9iDbSejZOW_RNZlFwK8LMqeZ-Q9cMJzeqpsstPQbw_txTScWYFHjZZ1720rlpYmxXzrNQRSfTDC9P0eKXABbMHAz36Lh7-g-igGWpGhmWIHkjcd9ecfJ1FSBxmmFonKCYLc4RFE5SNMWyFMYxDLWlMAzhmyKYi6ztsip53wpQ1I_1ivAjmIOqftR5VtYw_vEHrP34_V9RmP1M:1tduyX:F5t_wpUT7mOZm3YJv6o4YvHm6ccq2fJDbVBzn4BrtEQ'
+
+CSRFMIDDLEWARE='hdGP3cZGWvnvu9A5aH3m3MEY8nPh5LuZ8Xd3IAQBpi87TaHVm7goGoudqQAnLN54'
+
+BASE_URL = 'http://ws.audioscrobbler.com/2.0/'
+
+def get_all_scrobbles():
+    page = 1
+    scrobbles = []
+    while True:
+        print(f"Parsing page {page}; Total scrobbles: {len(scrobbles)}")
+        params = {
+            'method': 'user.getrecenttracks',
+            'user': USERNAME,
+            'api_key': API_KEY,
+            'format': 'json',
+            'page': page,
+            'limit': 200
+        }
+        response = requests.get(BASE_URL, params=params)
+        data = response.json()
+        if 'recenttracks' not in data or 'track' not in data['recenttracks']:
+            break
+        scrobbles.extend(data['recenttracks']['track'])
+        if int(data['recenttracks']['@attr']['page']) >= int(data['recenttracks']['@attr']['totalPages']):
+            break
+        page += 1
+        time.sleep(0.25) # respect API!
+    return scrobbles
+
+def read_scrobbles_from_file(filename='scrobbles.json'):
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            scrobbles = json.load(file)
+        print(f"Scrobbles have been read from {filename}")
+        return scrobbles
+    except FileNotFoundError:
+        print(f"File {filename} not found.")
+        return []
+    except json.JSONDecodeError:
+        print(f"File {filename} contains invalid JSON.")
+        return []
+
+def write_scrobbles_to_file(scrobbles, filename='scrobbles.json'):
+    with open(filename, 'w', encoding='utf-8') as file:
+        json.dump(scrobbles, file, ensure_ascii=False, indent=4)
+    print(f"Scrobbles have been written to {filename}")
+
+def count_artist_listens(scrobbles):
+    artist_counts = {}
+    for scrobble in scrobbles:
+        artist = scrobble['artist']['#text']
+        if artist in artist_counts:
+            artist_counts[artist] += 1
+        else:
+            artist_counts[artist] = 1
+    return artist_counts
+
+def filter_shitty_scrobbles(scrobbles, artist_counts):
+    shitty_scrobbles = []
+    for scrobble in scrobbles:
+        artist = scrobble['artist']['#text']
+        if artist_counts[artist] == 1:
+            shitty_scrobbles.append(scrobble)
+    return shitty_scrobbles
+
+def delete_scrobble(scrobble):
+    referer = 'https://www.last.fm/user/{USERNAME}'
+    headers = {
+        'Referer': referer,
+        'User-Agent': USER_AGENT,
+    }
+    cookies = {
+        'sessionid': SESSIONID,
+        'csrftoken': CSRFTOKEN
+    }
+    data = {
+        'artist_name': scrobble['artist']['#text'],
+        'track_name': scrobble['name'],
+        'timestamp': scrobble['date']['uts'],
+        'csrfmiddlewaretoken': CSRFMIDDLEWARE
+    }
+    response = requests.post(f'https://www.last.fm/user/{USERNAME}/library/delete', data=data, headers=headers, cookies=cookies)
+    time.sleep(0.25) # respect API!
+    print(f'{scrobble["name"]}: {response.status_code}')
+    return response.status_code == 200
+
+def write_scrobbles_to_file_readable(scrobbles, output_file='super_shitty_scrobbles.txt'):
+    with open(output_file, 'w', encoding='utf-8') as file:
+        for scrobble in scrobbles:
+            artist = scrobble['artist']['#text']
+            track_name = scrobble['name']
+            file.write(f"{artist}/{track_name}\n")
+
+def write_filtered_scrobbles_to_file(scrobbles, output_file='filtered_super_shitty_scrobbles.txt'):
+    with open(output_file, 'w', encoding='utf-8') as file:
+        for scrobble in scrobbles:
+            file.write(f"{scrobble}\n")
+
+def filter_ollama(client, data): # i tried using llm to track names :d 50/50 kinda
+    filtered_names = []
+    dropped_names = []
+    for line in data:
+        response = client.generate(
+            model='llama3', 
+            prompt=f"Does this title belong to a music or non-music content? Respond only with 'music' or 'non-music': {line}",
+        )
+        is_music = not "non-music" in response.response.lower()
+        if is_music:
+            filtered_names.append(line.strip())
+        else:
+            dropped_names.append(line.strip())
+        print(f'{line.strip()} is{" not" if not is_music else ""} music')
+    return filtered_names, dropped_names
+
+def read_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        return file.readlines()
+
+def main():
+    all_scrobbles = get_all_scrobbles();
+    # if you parsed all of your scrobbles already you can read it from file
+    # if you don't wan't to wait or to disturb api
+    # all_scrobbles = read_scrobbles_from_file('scrobbles.json')
+
+    write_scrobbles_to_file(all_scrobbles, 'scrobbles.json')
+    artist_counts = count_artist_listens(all_scrobbles)
+    shitty_scrobbles = filter_shitty_scrobbles(all_scrobbles, artist_counts)
+    write_scrobbles_to_file(shitty_scrobbles, 'shitty_scrobbles.json')
+    write_scrobbles_to_file_readable(shitty_scrobbles, 'super_shitty_scrobbles.txt') # in human-readable format hopefully
+
+    input(f'You are about to delete {len(shitty_scrobbles)} precious scrobble!!!!!!!!!!\nPress enter to continue..........................')
+    input(f'ARE YOU REALLY SURE?????')
+
+    cleaned_scrobbles = []
+    failed_scrobbles = []
+    for scrobble in shitty_scrobbles:
+        if delete_scrobble(scrobble):
+            cleaned_scrobbles.append(scrobble)
+        else:
+            failed_scrobbles.append(scrobble)
+    print(f'Failed: {len(failed_scrobbles)} Succeed: {len(cleaned_scrobbles)}')
+    write_scrobbles_to_file(cleaned_scrobbles, 'cleaned_scrobbles.json')
+    write_scrobbles_to_file(failed_scrobbles, 'failed_scrobbles.json') # you can retry
+    
+
+if __name__ == '__main__':
+    main()
